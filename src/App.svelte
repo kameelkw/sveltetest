ec6f851efaddebedf87a00b3b33000e60487c8fe
kameelkw <limak2688@gmail.com>
2022-02-11 21:50:33 +0100

    Added basic functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,84 @@
 <script>
-	export let name;
+  let newTodo = ""
+  let todos = [
+    {
+      title: "Get up",
+      done: true,
+    },
+    {
+      title: "Eat",
+      done: true,
+    },
+    {
+      title: "Go with vinky and kozi and rume and mima",
+      done: false,
+    },
+    {
+      title: "Clean floor",
+      done: false,
+    },
+    {
+      title: "Pet pussi",
+      done: true,
+    },
+  ]
+
+  function addTodo() {
+    if (newTodo === "") return
+
+    todos = [
+      ...todos,
+      {
+        title: newTodo,
+        done: false,
+      },
+    ]
+    newTodo = ""
+  }
+
+  function removeTodo(i) {
+    todos.splice(i, 1)
+    todos = todos
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>TODO</h1>
+  <section class="add-todo">
+    <input type="text" placeholder="task" bind:value={newTodo} />
+    <button disabled={newTodo === ""} on:click={addTodo}>Add</button>
+  </section>
+  <section class="todos">
+    {#each todos as todo, i}
+      <div class="todo">
+        <input type="checkbox" bind:checked={todo.done} name="complete task" />
+        <input type="text" disabled class:done={todo.done} value={todo.title} />
+        <button on:click={() => removeTodo(i)}>x</button>
+      </div>
+    {/each}
+  </section>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  .add-todo {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    gap: 0.5rem;
+  }
+  .todo {
+    display: grid;
+    grid-template-columns: auto 1fr auto;
+    align-items: center;
+    gap: 0.5rem;
+  }
+  input:disabled {
+    border: none;
+    background: lightpink;
+    color: black;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  input:disabled.done {
+    opacity: 0.4;
+    text-decoration: line-through;
+  }
 </style>
